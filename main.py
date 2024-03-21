83437b3aeb4e209a128575423ff5f82a852a9f28
JESAISap <nicolas.strekowski@gmail.com>
2024-03-21 21:25:57 +0100

    chaged something

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import tkinter as tk
-from tkinter import ttk, Tk, Button
+from tkinter import ttk, Tk
 
 class MainApp(Tk):
 
     def __init__(self):
 
+        #initializes Tk
         super().__init__()
         
+        # sets window default size
         self.geometry("720x420")
 
-        self.exitButton = Button(self, text="Quitter", command=self.destroy, bg="red")
+        self.exitButton = tk.Button(self, text="Bouh", command=self.destroy, bg="red", font=("Helvetica", 40))
         self.exitButton.pack(expand=1, fill="both")
 
 
 if __name__ == "__main__":
     App = MainApp()
     App.mainloop()
+    print("hello")
