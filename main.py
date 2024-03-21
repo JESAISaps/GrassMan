250473f182b088771a8bdb3274e5a96db2b771ef
JESAISap <nicolas.strekowski@gmail.com>
2024-03-21 19:20:10 +0100

    Changed way window works, now derives from Tk

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Tk
 
-class MainApp():
+class MainApp(Tk):
 
     def __init__(self):
-        self.root = tk.Tk()
-        self.frame = ttk.Frame(self.root, padding=5)
 
-        self.root.mainloop()
+        super().__init__()
+        
+        self.geometry("720x420")
+
+        self.exitButton = ttk.Button(self, text="Quitter", command=self.destroy)
+        self.exitButton.pack(side="bottom")
+
 
 if __name__ == "__main__":
-    MainApp()
+    App = MainApp()
+    App.mainloop()
