33d71dacc98da5ba30d77890f39aa753457a70d7
JESAISap <nicolas.strekowski@gmail.com>
2024-05-25 18:56:54 +0200

    Nettoyage de stade.py

--- stades.py
+++ stades.py
@@ -1,18 +1,15 @@
-from random import randint, choice
+from random import randint, choice, uniform
 from math import floor
 import sqlite3
 class Stade:
 
-    def __init__(self, nom:str, dimensions, saison) -> None:
+    def __init__(self, nom:str, dimensions) -> None:
         self.nom = nom
         self.longueur = 100
         self.largeur = 50
-        self.temperature1 = self.CreateFirstTempMap(saison)
+        self.temperature1 = self.CreateTemp([1,0,2018])
         self.ensoleillement = self.soleil()
         self.meteo = self.createMeteo()
-        self.isRoofClosed = self.changeRoofState()
-        self.chauffage = self.gestionchauffage()
-        self.arrosage = self.gestionarrosage()
 
         self.capteurs = [[(i, j) for j in range(dimensions[0]) for i in range(dimensions[1])]]
 
@@ -21,26 +18,6 @@ class Stade:
         Cree une matrice nulle de dimension yx
         """
         return [[0] * x for _ in range(y)]
-
-
-    def changeRoofState(self):
-        """
-        Retourne Vrai si on ferme le stade et Faux sinon
-        """
-        if self.meteo == "pluie" or self.meteo == "neige":
-            return True
-        else :
-            return False
-        
-    def gestionchauffage(self):
-        """
-        Retourne Vrai si on chauffe le stade et Faux sinon
-        """
-        for i in range (len(self.temperature1)):
-                    for k in range(len(self.temperature1[i])):
-                        if (self.temperature1[i][k]) <=10 :
-                            return True 
-        return False
     
     def gestionarrosage(self):
         """
@@ -51,72 +28,15 @@ class Stade:
                         if (self.temperature1[i][k]) >=25 :
                             return True 
         return False
- 
-    def CreateFirstTempMap(self,saison):
-        """
-        Crée une matrice des temperatures
-        Chaque élement représente 1 m carré
-        crée de manière à être le plus réaliste possible, mais ne prend pas en compte
-        la matrice précédente.
-        """
-        listeTemp = self.createBlankStadium(self.longueur, self.largeur)
-        if saison=="hiver":
-             Temp0 = randint(-10,10)
-        elif saison=="automne" or saison=="printemps":
-             Temp0 = randint(10,20)
-        elif saison=="été":
-             Temp0 = randint(20,35)
-             
-        listeTemp[0][0]=Temp0
-
-        # tempMax limite la variation de temperature a 2 * la temperature de depart sur l'ensemble du terrain
-        tempMax = abs(Temp0 +10)
-        for o in range(len(listeTemp)):                
-            for k in range(len(listeTemp[o])):
-                # on va faire une temperature qui depend de celles créées précédement, à proximité de l'element actuel
-                # cas limites aux bord de la matrice
-                if o==0 and k==0:
-                    temperature=Temp0
-                elif o==0:
-                    temperature = listeTemp[o][k-1]+randint(-1,1)
-                elif k==0:
-                    temperature = sum([listeTemp[o-1][k], listeTemp[o-1][k+1]])/2+randint(-1,1)
-                elif k==len(listeTemp[o])-1:
-                    temperature = sum([listeTemp[o-1][k-1], listeTemp[o-1][k],listeTemp[o][k-1]])/3+randint(-1,1)
-                else : 
-                    temperature = sum([listeTemp[o-1][k-1], listeTemp[o-1][k],listeTemp[o][k-1],listeTemp[o-1][k+1]])/4+randint(-1,1)
-                    
-                if abs(temperature) > abs(tempMax):
-                     # On ajuste temperature a tempMax si ça depasse et on lui redonne son signe
-                     temperature = tempMax * temperature/abs(temperature)
-
-                if temperature - floor(temperature) <= .5:
-                    listeTemp[o][k] = floor(temperature) + .5
-                else:
-                    listeTemp[o][k] = floor(temperature) 
-        return(listeTemp)
     
-    def moyenneTemp(self,listetemperature):
-        somme = 0
-        nombre_valeurs = 0
-        for k in range(len(listetemperature)):
-            for valeur in range(len(listetemperature[k])):
-                somme=(somme+listetemperature[k][valeur])
-                nombre_valeurs = nombre_valeurs + 1
-        moyenne = somme/nombre_valeurs
-        return(moyenne)
-    
-
-    def soleil(self):
-        """
-        Fonction qui va pas tarder a degager
-        """
-        listeSoleil = self.createBlankStadium(self.longueur, self.largeur)
-        for ligne in listeSoleil:
-            for k in range(len(ligne)):
-                    soleilHiver = randint(0,1)
-                    ligne[k] = soleilHiver
-        return(listeSoleil)
+    def CreateTemp(self,Date):
+        Moyenne=[3.7,4.4,8.1,11.7,15.6,20.2,22.6,22.1,18,13.6,8,4.5]
+        Mois=Date[1]
+        Jour=Date[0]
+        TempDepart=Moyenne[Mois]+uniform(-0.4,0.4)
+        if Mois!=11:
+            TempDepart=TempDepart+(Moyenne[Mois+1]+uniform(-0.2,0.2)-TempDepart)*Jour/31
+        return TempDepart
 
     def createMeteo(self):
         """""
@@ -130,37 +50,14 @@ class Stade:
         Modifie la météo, modifie la variable et adapte l'ouverture du toit si besoin
         """
         self.meteo = self.createMeteo()
-        self.isRoofClosed = self.changeRoofState()
 
     def modifSoleil(self):
         """
         Modifie la variable ensoleillement 
         """
         self.ensoleillement = self.soleil()
-
-    def modifTemp(self):
-        """
-        Modifie la variable temperature 
-        """
-        #self.temperature1 = self.temperature() # temperature modifiée aléatoirement
-        #print("starting modifying list")
-        if self.gestionchauffage() == True :
-             for k in range(len(self.temperature1)):
-                  for i in range(len(self.temperature1[k])):
-                        self.temperature1[k][i] = self.temperature1[k][i]+2
-        if self.gestionarrosage() == True :
-             for k in range(len(self.temperature1)):
-                  for i in range(len(self.temperature1[k])):
-                        self.temperature1[k][i] = self.temperature1[k][i]-2
         
         #print("done modifying list")
-
-
-    def getTemp(self):
-        """
-        retourne la variable temperature 
-        """
-        return self.temperature1
 
     def GetSoleil(self):
         """
@@ -179,12 +76,6 @@ class Stade:
         retourne la variable taille du stade sous la forme d'un tuple
         """
         return self.longueur, self.largeur
-    
-    def GetMooveRoof(self):
-        if self.meteo == "pluie" or self.meteo == "neige":
-            return("closed")
-        else :
-            return("open")
           
     def GetIdStade(self,bddstade):
         #bddstade=sqlite3.connect("./data/bddstade.db")
@@ -192,33 +83,10 @@ class Stade:
         #idstade=bdd.execute("Select IdStade from Stade where Nom='"+self.nom+"'").fetchall()
         #return idstade[0][0]
         return self.nom
-    
-    def associerdatetemperature(self,bddstade):
-        bdd=bddstade.cursor()
-        listejour=bdd.execute('Select Jour from Temperature').fetchall()
-        if listejour==[]:
-            return 1
-        else:
-            return listejour[len(listejour)-1][0]+1
-        
-    def recupidcapteur(self,x,y,idstade,bddstade):
-        bdd=bddstade.cursor()
-        idcapteur=bdd.execute('Select IdCapteurs from Capteurs where PositionX= '+str(x)+' and PositionY= '+str(y)+' and IdStade= '+str(idstade)).fetchall()
-        return idcapteur[0][0]
-        
-    def ImportTemperature(self,listetemp,bddstade,nomstade,idstade):
-        jour=self.associerdatetemperature(bddstade)
-        bdd=bddstade.cursor()
-        for ligne in range (len(listetemp)):
-            for colonne in range(len(listetemp[ligne])):
-                    idcapteur=self.recupidcapteur(ligne,colonne,idstade,bddstade)
-                    bdd.execute('INSERT INTO Temperature values ('+str(jour)+','+str(listetemp[ligne][colonne])+','+str(idcapteur)+')')
 
         
 
 # condition vraie seulement si ce script est celui qui a ete run, Faux si il est run dans un import
 if __name__ == "__main__":
-    s = Stade("Velodrome",(50,100),"hiver")
-    s.CreateFirstTempMap("hiver")
-    s.ImportTemperature(s.CreateFirstTempMap("hiver"),    bdd = sqlite3.connect("./data/bddstade.db"))
-    print(s.moyenneTemp(s.CreateFirstTempMap("hiver")))
+    s = Stade("Velodrome",(50,100))
+    print(s.CreateTemp((2,5)))
